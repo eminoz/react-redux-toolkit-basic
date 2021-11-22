544f044a971a970a6dbe09e8af85c47a5c3e9cb1
eminoz <eminoz@yahoo.com>
2021-11-22 22:09:12 +0300

    redux app changed with redux tool kit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
+
+import { counterActions } from "../store/index";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
@@ -6,16 +8,16 @@ const Counter = () => {
   const counter = useSelector((state) => state.counter); //this handle the state we choose
   const show = useSelector((state) => state.showCounter); //this handle the state we choose
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(10));//{type:SOME_UNIQUE_IDENTIFIER,PAYLOAD:10}
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,29 @@
-import { createStore } from "redux";
-
+import { createSlice, configureStore } from "@reduxjs/toolkit"; //toollkit daha basit redux kullanımı sağlar
 const initialState = { counter: 0, showCounter: true };
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+      //normalde current state değiştirilmemeli sadece override edilmeli.
+      //bu tool kit current stateti bir başka kütüphane kullanıp oraya snapshop ını
+      //kaydeder ve onun üzerine override eder
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "toggle") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
-  return state; //if does not any change return current state
-};
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+export const counterActions = counterSlice.actions;
 export default store;
